fc80e6fa69d47ffbae96336a47b8a17e8d836f4b
Danial Javady <danial.javady@yoppworks.com>
2023-04-04 17:40:38 -0400

    update

--- TensorLib/src/domain/Tensor.h
+++ TensorLib/src/domain/Tensor.h
@@ -7,73 +7,72 @@
 #include <functional>
 #include <memory>
 
-template <typename T>
-concept Ranked = requires {
-	typename T::rank_type;
+template<typename T>
+concept Ranked =
+requires {
 	typename T::size_type;
 };
 
-template <Ranked... Args>
-constexpr int acc_ranks() {
-	int sum = 0;
-	((sum += Args::rank_type::value), ...);
-	return sum;
+template<Ranked... Args>
+constexpr int acc_ranks()
+{
+	return (Args::rank + ...);
 }
 
-template <Ranked... Args>
-constexpr int acc_size() {
-	int sum = 0;
-	((sum += Args::size_type::value), ...);
-	return sum;
+template <typename... Ts>
+int acc_size(Ts&&... ts) {
+	auto get_size = [](auto& obj) { return obj.size(); };
+	return (0 + ... + get_size(ts));
 }
+
 
 template<int rank, Number T, bool isConst>
 requires (rank > 0)
 class TensorIterator;
 
-template<int rank, Number T>
-requires (rank > 0)
+template<int m_rank, Number T>
+requires (m_rank > 0)
 class Tensor
 {
  public:
 	using value_type = T;
 	using size_type = std::size_t;
-	using rank_type = std::integral_constant<int, rank>;
 	using reference = value_type&;
 	using const_reference = value_type const&;
 	using pointer = value_type*;
-	using iterator = TensorIterator<rank, T, false>;
-	using const_iterator = TensorIterator<rank, T, true>;
+	using iterator = TensorIterator<m_rank, T, false>;
+	using const_iterator = TensorIterator<m_rank, T, true>;
 
+	static constexpr int rank = m_rank;
 
 	explicit Tensor(size_type size)
-		: data_(std::make_shared<T[]>(size)),
+		: data_(std::shared_ptr<T[]>(new T[size], std::default_delete<T[]>())),
 		  size_(size)
 	{
 	}
 
 	Tensor(std::initializer_list<T> list)
-		: data_(std::make_shared<T[]>(list.size())),
+		: data_(std::shared_ptr<T[]>(new T[list.size()], std::default_delete<T[]>())),
 		  size_(list.size())
 	{
 	}
 
-	template<Ranked ... Ts>
+	template<typename ... Ts>
 	Tensor(Ts ... ts)
-	requires(acc_ranks<Ts...>() == rank_type::value)
-		: data_(std::make_shared<T[]>(acc_size<Ts...>())),
-		  size_(0)
+	requires (acc_ranks<Ts...>() == rank)
+	: data_(std::shared_ptr<T[]>(new T[acc_size(ts...)], std::default_delete<T[]>())),
+		  size_(acc_size(ts...))
 	{
 	}
 
 	explicit Tensor(T* data, int size)
-		: data_(std::make_shared<T[]>(size)),
+		: data_(std::shared_ptr<T[]>(new T[size], std::default_delete<T[]>())),
 		  size_(size)
 	{
 	}
 
 	Tensor(const Tensor& other)
-		: data_(std::make_shared<T[]>(other.size())),
+		: data_(std::shared_ptr<T[]>(new T[other.size()], std::default_delete<T[]>())),
 		  size_(other.size_)
 	{
 		std::copy(other.begin(), other.end(), data_.get());
