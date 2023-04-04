6aef8cfe456491a4de1b24161a57fd3df09cb075
ZelboK <danialjavady96@gmail.com>
2023-04-03 22:18:42 -0400

    incorporate rank into tensor

--- TensorLib/cmake-build-debug/_deps/googletest-build/googletest/CTestTestfile.cmake
+++ TensorLib/cmake-build-debug/_deps/googletest-build/googletest/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /Users/danialjavady/Desktop/ksm/TensorLib/TensorLib/cmake-build-debug/_deps/googletest-src/googletest
-# Build directory: /Users/danialjavady/Desktop/ksm/TensorLib/TensorLib/cmake-build-debug/_deps/googletest-build/googletest
+# Source directory: C:/Users/12893/Desktop/TensorLib/TensorLib/cmake-build-debug/_deps/googletest-src/googletest
+# Build directory: C:/Users/12893/Desktop/TensorLib/TensorLib/cmake-build-debug/_deps/googletest-build/googletest
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
