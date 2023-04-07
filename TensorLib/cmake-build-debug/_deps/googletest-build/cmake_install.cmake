4ba06251eaae7311548504042b5d27512453af8b
Danial Javady <danial.javady@yoppworks.com>
2023-04-07 13:30:02 -0400

    fix errors

--- TensorLib/cmake-build-debug/_deps/googletest-build/cmake_install.cmake
+++ TensorLib/cmake-build-debug/_deps/googletest-build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/12893/Desktop/TensorLib/TensorLib/cmake-build-debug/_deps/googletest-src
+# Install script for directory: /Users/danialjavady/Desktop/ksm/TensorLib/TensorLib/cmake-build-debug/_deps/googletest-src
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/TensorLib")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,12 +34,12 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2022.3.1/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "/Library/Developer/CommandLineTools/usr/bin/objdump")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("C:/Users/12893/Desktop/TensorLib/TensorLib/cmake-build-debug/_deps/googletest-build/googlemock/cmake_install.cmake")
+  include("/Users/danialjavady/Desktop/ksm/TensorLib/TensorLib/cmake-build-debug/_deps/googletest-build/googlemock/cmake_install.cmake")
 
 endif()
 
