737884e04e2cbe3cdec07a33dadcc3945c98f6f6
ZelboK <danialjavady96@gmail.com>
2023-04-25 15:28:11 -0400

    unit tests

--- TensorLib/cmake-build-debug/src/tests/CTestTestfile.cmake
+++ TensorLib/cmake-build-debug/src/tests/CTestTestfile.cmake
@@ -5,5 +5,5 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(TensorLibTests "/Users/danialjavady/Desktop/ksm/TensorLib/TensorLib/cmake-build-debug/src/tests/TensorLibTests")
-set_tests_properties(TensorLibTests PROPERTIES  _BACKTRACE_TRIPLES "/Users/danialjavady/Desktop/ksm/TensorLib/TensorLib/src/tests/CMakeLists.txt;13;add_test;/Users/danialjavady/Desktop/ksm/TensorLib/TensorLib/src/tests/CMakeLists.txt;0;")
+set_tests_properties(TensorLibTests PROPERTIES  _BACKTRACE_TRIPLES "/Users/danialjavady/Desktop/ksm/TensorLib/TensorLib/src/tests/CMakeLists.txt;14;add_test;/Users/danialjavady/Desktop/ksm/TensorLib/TensorLib/src/tests/CMakeLists.txt;0;")
 subdirs("../../_deps/googletest-build")
