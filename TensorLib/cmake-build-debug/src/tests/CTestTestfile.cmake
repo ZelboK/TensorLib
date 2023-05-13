da8718a148b13f2cba138bf427be0134612a56cb
ZelboK <danialjavady96@gmail.com>
2023-05-13 18:35:47 -0400

    Clean up project structure

--- TensorLib/cmake-build-debug/src/tests/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/danialjavady/Desktop/ksm/TensorLib/TensorLib/src/tests
-# Build directory: /Users/danialjavady/Desktop/ksm/TensorLib/TensorLib/cmake-build-debug/src/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TensorLibTests "/Users/danialjavady/Desktop/ksm/TensorLib/TensorLib/cmake-build-debug/src/tests/TensorLibTests")
-set_tests_properties(TensorLibTests PROPERTIES  _BACKTRACE_TRIPLES "/Users/danialjavady/Desktop/ksm/TensorLib/TensorLib/src/tests/CMakeLists.txt;14;add_test;/Users/danialjavady/Desktop/ksm/TensorLib/TensorLib/src/tests/CMakeLists.txt;0;")
-subdirs("../../_deps/googletest-build")
