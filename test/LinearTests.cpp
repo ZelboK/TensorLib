de463741d3209e499b3a570355efe901301b801f
ZelboK <danialjavady96@gmail.com>
2023-06-05 21:03:30 -0400

    small comment on tests

--- test/LinearTests.cpp
+++ test/LinearTests.cpp
@@ -2,7 +2,10 @@
 // Created by Danial javady on 2023-03-12.
 //
 #include <catch2/catch_test_macros.hpp>
-//TODO
+// TODO refactor the tests existing inside of src
+// to be here for better structuring
+// Things are the way they are atm because I was learning
+// more about CMake and how to structure projects
 TEST_CASE("Main") {
 
 }
